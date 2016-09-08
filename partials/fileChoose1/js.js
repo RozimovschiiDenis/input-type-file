5359d01543662bb42e7d52be5ed4816046b7c338
Denis Rozimovschii <rozimovschii.denis@yahoo.com>
2016-09-08 10:47:12 +0300

    Fixed js code, added partials

---
+++ partials/fileChoose1/js.js
@@ -0,0 +1,19 @@
+
+var FileSelectCustom = {
+    init: function() {
+        document.addEventListener("DOMContentLoaded", function() {
+            var selects = document.getElementsByClassName("file-input");
+            for (var i = 0; i<selects.length; i++) {
+
+                selects[i].onchange = function() {
+                    var id = this.getAttribute("data-box");
+                    var label = document.getElementsByClassName(id + "-label")[0]
+                    label.innerHTML = this.files[0].name;
+                }
+            }
+        });
+    }
+};
+(function() {
+    FileSelectCustom.init();
+})();
